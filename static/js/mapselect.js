0ef8c95e68ee087c4cab40ce49ed3eb205ebd93b
Willem Klumpenhouwer <willemjjk@gmail.com>
2020-09-11 09:18:09 -0400

    Started work on different UI approach to map layer themes

---
+++ static/js/mapselect.js
@@ -0,0 +1,83 @@
+var TimePeriods
+
+var mapStates = {
+	"USA": {
+		"California": {
+			"Los Angeles": ["AM Peak", "Weekday PM", "90003", "90004"],
+			"San Diego": ["92093", "92101"]
+		},
+		"Texas": {
+			"Dallas": ["75201", "75202"],
+			"Austin": ["73301", "73344"]
+		}
+	},
+	"India": {
+		"Assam": {
+			"Dispur": ["781005"],
+			"Guwahati" : ["781030", "781030"]
+		},
+		"Gujarat": {
+			"Vadodara" : ["390011", "390020"],
+			"Surat" : ["395006", "395002"]
+		}
+	}
+}
+
+
+window.onload = function () {
+    console.log("ON LOAD")
+	
+	//Get html elements
+	var countySel = document.getElementById("countySel");
+	var stateSel = document.getElementById("stateSel");	
+	var citySel = document.getElementById("citySel");
+	var zipSel = document.getElementById("zipSel");
+	
+	//Load countries
+	for (var country in countryStateInfo) {
+        console.log(country)
+		countySel.options[countySel.options.length] = new Option(country, country);
+	}
+	
+	//County Changed
+	countySel.onchange = function () {
+		 
+		 stateSel.length = 1; // remove all options bar first
+		 citySel.length = 1; // remove all options bar first
+		 zipSel.length = 1; // remove all options bar first
+		 
+		 if (this.selectedIndex < 1)
+			 return; // done
+		 
+		 for (var state in countryStateInfo[this.value]) {
+			 stateSel.options[stateSel.options.length] = new Option(state, state);
+		 }
+	}
+	
+	//State Changed
+	stateSel.onchange = function () {		 
+		 
+		 citySel.length = 1; // remove all options bar first
+		 zipSel.length = 1; // remove all options bar first
+		 
+		 if (this.selectedIndex < 1)
+			 return; // done
+		 
+		 for (var city in countryStateInfo[countySel.value][this.value]) {
+			 citySel.options[citySel.options.length] = new Option(city, city);
+		 }
+	}
+	
+	//City Changed
+	citySel.onchange = function () {
+		zipSel.length = 1; // remove all options bar first
+		
+		if (this.selectedIndex < 1)
+			return; // done
+		
+		var zips = countryStateInfo[countySel.value][stateSel.value][this.value];
+		for (var i = 0; i < zips.length; i++) {
+			zipSel.options[zipSel.options.length] = new Option(zips[i], zips[i]);
+		}
+	}	
+}
